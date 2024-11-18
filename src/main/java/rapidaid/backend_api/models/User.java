5dd0efcaabe2b4d50346f6d531e11c8bc9b8def8
Materdelo <113280384+Materdelo@users.noreply.github.com>
2024-11-18 19:25:30 +0100

    fix/add Authorisation Prefix

--- src/main/java/rapidaid/backend_api/models/User.java
+++ src/main/java/rapidaid/backend_api/models/User.java
@@ -38,7 +38,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.getRole()));
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.getRole()));
     }
 
     @Override
